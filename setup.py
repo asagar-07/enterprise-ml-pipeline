c59ce896c0ac69e94c9b7868dd6dba4305f92c57
asagar-07 <24895773+asagar-07@users.noreply.github.com>
2026-03-05 13:25:42 -0500

    chore: clean up tracking and updated gitignore updated local package setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,17 @@
 import setuptools
 from pathlib import Path
+from typing import List
+
+HYPEN_E_DOT = "-e ."
+
+def get_requirements(file_path: str) -> List[str]:
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
 
 __version__ = "0.0.0"
 
